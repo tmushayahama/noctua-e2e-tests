0e29ba8cf40899db359217f82f154b4d4f972f54
tmushayahama <tmushayahama@gmail.com>
2024-01-31 23:10:03 -0800

    Added creation on a standard annotation

--- cypress/e2e/noctua-standard-annotations/create-enables.cy.ts
+++ cypress/e2e/noctua-standard-annotations/create-enables.cy.ts
@@ -7,12 +7,35 @@ describe('Create a Standard Annotation', () => {
 
   before(() => {
     annotationPage.visit(createAnnotationsData.modelId);
+
+    cy.get('body').then(($body) => {
+      if ($body.find('[data-cy="noc-login-button"]').length > 0) {
+        cy.get('[data-cy="noc-login-button"]').click();
+
+        //cy.url().should('not.eq', 'http://the-previous-url.com');
+        // or, if you know the new URL:
+        // cy.url().should('include', 'http://the-new-url.com');
+
+        cy.get('a').contains('Sign in locally').should('be.visible').click();
+        cy.get('input[name="username"]').should('be.visible').type('gotremayne');
+        cy.get('input[name="password"]').should('be.visible').type('artist12345');
+        cy.get('button').contains('Submit').click();
+
+        cy.wait(1000);
+        cy.get('#return-trip-login').should('be.visible').click();
+
+      } else {
+        cy.log('Login button not found');
+      }
+    });
   });
 
   it('Types into a textarea and checks autocomplete', () => {
     createAnnotationsData.annotations.forEach(data => {
       selectFromAutocomplete(`[data-cy="form-input-${data.target}"]`, data.input, data.optionId);
     });
+
+    // cy.get('[data-cy="save-annotation-button"]').should('be.visible').click();
   });
 
 });
--- cypress/support/utils/create-annotations.ts
+++ cypress/support/utils/create-annotations.ts
@@ -1,15 +1,19 @@
 export function selectFromAutocomplete(inputSelector: string, text: string, optionId: string = null): void {
-  cy.get(inputSelector).should('exist');
-  cy.get(`${inputSelector} textarea`).type(text);
+
+  cy.get(inputSelector).should('exist').click();
+  if (text) {
+    cy.get(`${inputSelector} textarea`).type(text);
+  }
 
   if (optionId) {
     cy.wait(1000);
     cy.get('mat-option').should('be.visible');
     cy.get('mat-option').each(($option) => {
-      if ($option.find('.noc-term-id').text().includes(optionId)) {
+      if ($option.find('.noc-term-id, .noc-term-label').text().includes(optionId)) {
         cy.wrap($option).click();
       }
     });
   }
+
 
 }
